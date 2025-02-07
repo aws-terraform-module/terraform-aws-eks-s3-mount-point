ea33de7f9b2ffd7ca85550d0d3011eb0199efc86
Mr Nim <50592567+mrnim94@users.noreply.github.com>
2025-02-07 09:43:01 +0000

    udpate provider kubernetes

--- c3-01-helm-provider.tf
+++ c3-01-helm-provider.tf
@@ -11,3 +11,10 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
+
+# Kubernetes Provider Block
+provider "kubernetes" {
+  host                   = var.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
