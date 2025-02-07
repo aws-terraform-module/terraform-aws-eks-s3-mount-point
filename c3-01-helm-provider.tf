7e9807374f167e07475c5e99eb08b7ff18ba9697
Mr Nim <50592567+mrnim94@users.noreply.github.com>
2025-02-07 10:09:03 +0000

    remove

--- c3-01-helm-provider.tf
+++ c3-01-helm-provider.tf
@@ -11,10 +11,3 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
-
-# Kubernetes Provider Block
-provider "kubernetes" {
-  host                   = var.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
